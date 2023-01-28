ef6aa234351c4e490e390162cd8888f7531ffd74
satomizawazawa <viva3sx@gmail.com>
2023-01-28 19:04:47 +0900

    修正２

--- src/Review01.java
+++ src/Review01.java
@@ -3,23 +3,21 @@ public class Review01 {
 
     public static void main(String[] args) {
         // 商品価格を入れるint型変数の宣言
-        int withoutTax = 1500;
-        // 消費税額を入れるdouble型変数の宣言
-        double taxAmount;
+        int withoutTax = 1450;
         // taxメソッドの呼び出し
-        taxAmount = tax(withoutTax);
+        int taxAmount = tax(withoutTax);
         //税込価格を入れるint型変数の宣言、double型の消費税額をint型へ変換
-        int withTax = withoutTax + (int) taxAmount;
+        int withTax = withoutTax + taxAmount;
 
-        System.out.println(withoutTax + "円の商品の税込価格は" + withTax + "円（消費税は" + (int) taxAmount + "円）です。");
+        System.out.println(withoutTax + "円の商品の税込価格は" + withTax + "円（消費税は" + taxAmount + "円）です。");
 
     }
 
 //int型の商品価格を受け取り、消費税額を戻すtaxメソッド
-    public static double tax(double withoutTax) {
+    public static int tax(int withoutTax) {
         // double型変数taxAmountへ消費税額を代入
         double taxAmount = withoutTax * 0.1;
-        return taxAmount;
+        return (int)taxAmount;
     }
 
 }
